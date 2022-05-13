f0fef4658153d38f5305d2e24d25638556c13150
Артем Куканов <artemkukanov@yandex.ru>
2022-05-13 15:35:16 +0300

    Неполадки с тегами

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import get_object_or_404, render
+
 from .models import Post
 from .models import Group
+from django.conf import settings
 
 
 def index(request):
     template = 'posts/index.html'
-    text = Post.objects.order_by('-pub_date')[:10]
+    text = Post.objects.order_by('-pub_date')[:settings.CONST]
     context = {
         'text': text,
     }
@@ -14,10 +16,16 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group
+                                ).order_by('-pub_date')[:settings.CONST]
     template = 'posts/group_list.html'
     context = {
         'group': group,
         'posts': posts,
     }
     return render(request, template, context)
+
+    # Для ревьера: в третьем спринте не было информации о том,
+    # как избавляться от сортировки order_by (скорее всего,
+    # что это тоже из 4-го спринта, который мне только предстоит).
+    # Константу [:10] я вынес в settings.py и импортировал.
